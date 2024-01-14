91ede3d3823bc7344ad2cbc70525e5eddfeb06dc
Marc Scattolin <marcscattolin@gmail.com>
2024-01-14 09:57:20 -0500

    add date to display

--- src/app/clock.tsx
+++ src/app/clock.tsx
@@ -3,10 +3,14 @@ import styles from './clock.module.css'
 import {useEffect, useState} from 'react';
 
 export default function ClockDisplay() {
-    const [date, setDate] = useState(getTimeString());
+    const [time, setTime] = useState(getTimeString());
+    const [date, setDate] = useState(getDateString());
 
     useEffect(() => {
-        const timerId = setInterval(() => {setDate(getTimeString());}, 1000);
+        const timerId = setInterval(() => {
+            setTime(getTimeString());
+            setDate(getDateString());
+        }, 1000);
         return function cleanup() {
             clearInterval(timerId);
         }
@@ -14,11 +18,16 @@ export default function ClockDisplay() {
 
     return (
         <>
-        <h1 className={styles.clock} suppressHydrationWarning>{date}</h1>
+        <div className={styles.clockDate} suppressHydrationWarning>{date}</div>
+        <h1 className={styles.clock} suppressHydrationWarning>{time}</h1>
         </>
     );
 }
 
 function getTimeString(): string {
     return new Date().toLocaleTimeString([], {hour: 'numeric', minute: 'numeric'});
+}
+
+function getDateString(): string {
+    return new Date().toLocaleDateString([], { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'});
 }
